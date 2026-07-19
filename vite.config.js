418992d75dc19294d10da9a3ccba47bea3c33d2a
M.Manasiga <manasiga2006@gmail.com>
2026-07-19 12:57:26 +0530

    fix build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-const repoName = 'zena-chatbot-react';
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
+  base: '/zena-chatbot--npm/',
   server: {
     port: 5173,
     host: true,
   },
-});
+})
